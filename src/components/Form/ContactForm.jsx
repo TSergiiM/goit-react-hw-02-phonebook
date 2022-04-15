c33f544d8bccaa2c140b92c66839936cd79721ac
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-15 09:22:04 +0300

    Update ContactForm.jsx

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -16,15 +16,19 @@ export class ContactForm extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input
-          value={this.state.message}
-          onChange={this.handleChange}
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        ></input>
-        <button type="submit">add</button>
+        <h1>Phonebook</h1>
+        <label htmlFor="name">
+          Name
+          <input
+            value={this.state.message}
+            onChange={this.handleChange}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          ></input>
+        </label>
+        <button type="submit">Add contact</button>
       </form>
     );
   }
